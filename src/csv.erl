379cd80223d2d64a0bd8417db968d0d939f1af49
mats cronqvist <masse@cronqvi.st>
2017-06-15 22:17:17 +0200

    refactor states

--- src/csv.erl
+++ src/csv.erl
@@ -17,6 +17,21 @@
 %%    EOL = ( %x0D / %x0A / %x0D %x0A )
 %%    TEXTDATA =  %x20-21 / %x23-2B / %x2D-7E
 
+%% file        = record *eol-record [EOL]
+%% eol-record  = EOL record
+%% record      = field *comma-field                           : pop().
+%% comma-field = COMMA field
+%% field       = escaped / non-escaped
+%% escaped     = DQUOTE *qchar DQUOTE                         : push(2).
+%% non-escaped = *TEXTDATA                                    : push(1).
+%% qchar       = DQCHAR / SQCHAR
+%% DQCHAR      = %x22 %x22                                    : sub(34).
+%% SQCHAR      = %x0A / %x0D / %x20-21 / %x23-7E
+%% COMMA       = %x2C
+%% DQUOTE      = %x22
+%% EOL         = %x0D %x0A / %x0D / %x0A
+%% TEXTDATA    = %x20-21 / %x23-2B / %x2D-7E
+
 -module(csv).
 
 -export([fold/3, parse/1]).
@@ -39,13 +54,14 @@ s_file(S0) ->
   S1 = once(S0, fun s_record/1),
   many(S1, fun s_eol_record/1).
 
-s_record(S0) ->
-  S1 = once(S0, fun s_field/1),
-  callback(many(S1, fun s_comma_field/1)).
-
 s_eol_record(S0) ->
   S1 = once(S0, fun l_EOL/1),
   once(S1, fun s_record/1).
+
+s_record(S0) ->
+  S1 = once(S0, fun s_field/1),
+  S2 = many(S1, fun s_comma_field/1),
+  pop(S2).
 
 s_comma_field(S0) ->
   S1 = once(S0, fun l_COMMA/1),
@@ -57,23 +73,34 @@ s_field(S0) ->
 
 s_escaped(S0) ->
   S = once(S0, fun l_DQUOTE/1),
-  S1 = many_stash(S, fun l_QCHAR/1),
+  S1 = many_stash(S, fun s_qchar/1),
   once(S1, fun l_DQUOTE/1).
 
 s_non_escaped(S0) ->
   many_stash(S0, fun l_TEXTDATA/1).
 
+s_qchar(S0) ->
+  Choices = [fun l_SQCHAR/1, fun l_DQCHAR/1],
+  choose(S0, Choices).
+
 %%-----------------------------------------------------------------------------
 
-l_QCHAR(State0) ->
+l_DQCHAR(State0) ->
   {State1, Char0} = peek(State0),
   if
     (Char0 =:= $") ->
       {State2, Char1} = peek(State1),
       if
-        Char1 =:= $" -> hit(State2, Char1);
+        Char1 =:= $" -> hit(State2, 34);
         true -> miss(backup(State2))
       end;
+    true ->
+      miss(State1)
+  end.
+
+l_SQCHAR(State0) ->
+  {State1, Char0} = peek(State0),
+  if
     (Char0 =:= $\n) orelse
     (Char0 =:= $\r) orelse
     ((16#20 =< Char0) andalso (Char0 =< 16#7E)) ->
@@ -137,9 +164,9 @@ hit(State, El) ->
 miss(State) ->
   backup(State#state{res=miss}).
 
-callback(State = #state{res=hit, cb_fun=Fun, cb_acc=Acc, stash=Stash}) ->
+pop(State = #state{res=hit, cb_fun=Fun, cb_acc=Acc, stash=Stash}) ->
   State#state{cb_acc=Fun(Stash, Acc), stash=[]};
-callback(State) ->
+pop(State) ->
   State.
 
 eof(#state{cb_fun=Fun, cb_acc=Acc}) ->
