6229abb185d0e9850060cd0a5151a582e10f7f8b
mats cronqvist <masse@cronqvi.st>
2022-12-06 20:12:24 +0000

    update template file

--- src/fatpage_preamble.erl
+++ src/fatpage_preamble.erl
@@ -2,20 +2,20 @@
 -module(fatpage_preamble).
 
 -export([file/2, string/2,
-        repeat/4, alternative/2, sequence/2, final/2]).
+        repeat/4, alternative/2, sequence/2, final/2, final/3]).
 
 -export(['-EOF-'/1, '-CR-'/1]).
 
 -record(obj, {ptr, bin, sz}).
 
 '-EOF-'(#obj{ptr = Ptr, sz = Z} = Obj) ->
-    if Z == Ptr -> {ok, eof, eof};
-       Ptr < Z -> {error, not_eof, Obj};
-       true -> {error, past_eof, eof}
+    if Z == Ptr -> {ok, eof, Obj#obj{ptr = eof}};
+       Ptr < Z -> {error, not_eof};
+       true -> {error, past_eof}
     end.
 
 '-CR-'(Obj) ->
-    final(<<10>>, Obj).
+    final(10, Obj).
 
 file(Filename, F) ->
     case file:read_file(Filename) of
@@ -33,47 +33,38 @@ string(B, F) ->
         {ok, Xs, O} when O#obj.ptr =:= eof -> {ok, Xs, eof};
         {ok, Xs, O} when O#obj.sz =:= O#obj.ptr -> {ok, Xs, 0};
         {ok, Xs, O} -> {ok, Xs, O#obj.sz-O#obj.ptr};
-        {error, R, #obj{ptr = P}} -> {error, R, P}
+        {error, R} -> {error, R}
     end.
-
--define(IS_EOF(O), O#obj.ptr =:= eof).
-%% should F return O in {error, R, O}?
 
 repeat(Min, Max, F, Obj) ->
     repeat(0, Min, minus1(Max), F, Obj, []).
 
-repeat(N, Min, Mx, F, Obj, Xs) ->
+repeat(N, Mn, Mx, F, Obj, Xs) ->
     case F(Obj) of
-        {ok, X, O} when N =:= Mx   -> {ok, lists:reverse([X|Xs]), O};
-        {ok, X, O}                 -> repeat(N+1, Min, Mx, F, O, [X|Xs]);
-        {error, R, O} when N < Min -> {error, {too_few, R}, O};
-        {error, _R, O}             -> {ok, lists:reverse(Xs), O}
+        {ok, X, O} when N =:= Mx -> {ok, lists:reverse([X|Xs]), O};
+        {ok, X, O}               -> repeat(N+1, Mn, Mx, F, O, [X|Xs]);
+        {error, R} when N < Mn   -> {error, {too_few, R}};
+        {error, _R}              -> {ok, lists:reverse(Xs), Obj}
     end.
 
 minus1(infinity) -> infinity;
 minus1(I) -> I-1.
 
-alternative(_, Obj) when ?IS_EOF(Obj) ->
-    {error, eof, Obj};
+alternative(_, #obj{ptr = eof}) ->
+    {error, eof};
 alternative(Fs, Obj) ->
     alternative(Fs, Obj, []).
 
-alternative([], Obj, Es) ->
-    {error, {fail, Es}, Obj};
+alternative([], _Obj, Es) ->
+    {error, {fail, Es}};
 alternative([F|Fs], Obj, Es) ->
-    case alt(F, Obj) of
+    case F(Obj) of
         {ok, X, O} -> {ok, X, O};
-        {error, R, O} -> alternative(Fs, Obj, [{R, O}|Es])
+        {error, E} -> alternative(Fs, Obj, [E|Es])
     end.
 
-alt(F, Obj) ->
-    case erlang:fun_info(F, arity) of
-        {arity, 1} -> final(F, Obj);
-        {arity, 2} -> F(Obj)
-    end.
-
-sequence(_, Obj) when ?IS_EOF(Obj) ->
-    {error, eof, Obj};
+sequence(_, #obj{ptr = eof}) ->
+    {error, eof};
 sequence(Fs, Obj) -> 
     sequence(Fs, Obj, []).
 
@@ -82,33 +73,45 @@ sequence([], Obj, Xs) ->
 sequence([F|Fs], Obj, Xs) ->
     case F(Obj) of
         {ok, X, O} -> sequence(Fs, O, [X|Xs]);
-        {error, R, O} -> {error, {unexpected, R}, O}
+        {error, R} -> {error, {unexpected, R}}
     end.
 
-final(Bin, Obj) when is_binary(Bin) ->
-    try peek_chars(Obj, Z = byte_size(Bin)) of
-        Bin -> {ok, Bin, bump_ptr(Obj, Z)};
-        E -> {error, {miss, E}, Obj}
-    catch
-        error:badarg -> {error, {miss, eof}, Obj}
-    end;
-final({Bin1, Bin2}, Obj) ->
-    try binary:part(Obj#obj.bin, {Ptr = Obj#obj.ptr, 1}) of
-        Bin when Bin1 =< Bin, Bin =< Bin2 -> {ok, Obj#obj{ptr = Ptr+1}};
-        E -> {error, {miss, E, Bin1, Bin2}, Obj}
-    catch
-        error:badarg -> {error, {miss, eof, Bin1, Bin2}, Obj}
-    end;
-final(GUARD, Obj) when is_function(GUARD, 1) ->
-    try GUARD(C = peek_chars(Obj, 1)) of
-        true -> {ok, C, bump_ptr(Obj, 1)};
-        false -> {error, {miss, C}, Obj}
-    catch
-        error:badarg -> {error, {miss, eof}, Obj}
+final(Char, Obj) when is_integer(Char) ->
+    case peek_chars(Obj, 1) of
+        {[Char], Bin, Sz} -> {ok, Bin, bump_ptr(Obj, Sz)};
+        E -> {error, {miss, E}}
+    end.
+final(C1, C2, Obj) when is_integer(C1), is_integer(C2) ->
+    case peek_chars(Obj, 1) of
+        {[C], Bin, Sz} when C1 =< C, C =< C2 ->
+            {ok, Bin, bump_ptr(Obj, Sz)};
+        E ->
+            {error, {miss, E, C1, C2}}
     end.
 
+peek_chars(#obj{ptr = Ptr, sz = Sz}, Num) when Sz < Ptr+Num ->
+    {error, eof};
+peek_chars(#obj{bin = Bin, ptr = Ptr}, 1) ->
+    case binary:part(Bin, {Ptr, 1}) of
+        <<0:1, C0:7>> = B ->
+            {[C0], B, 1};
+        <<6:3, C1:5>> = B1 ->
+            case binary:part(Bin, {Ptr+1, 1}) of
+                <<2:2,C2:6>> = B2 ->
+                    {[(C1 bsl 6)+C2], <<B1/binary, B2/binary>>, 2};
+                B2 ->
+                    error({unhandled_utf8, B1, B2})
+            end;
+        B0 ->
+            error({unhandled_utf8, B0})
+    end;
 peek_chars(#obj{bin = Bin, ptr = Ptr}, Num) ->
-    binary:part(Bin, {Ptr, Num}).
+    try unicode:characters_to_list(B = binary:part(Bin, {Ptr, Num})) of
+        Chars when Num =:= length(Chars) -> {Chars, B, Num};
+        {incomplete, H, T} -> error({unhandled_utf8, H, T});
+        Chars -> error({unhandled_utf8, Chars})
+    catch
+        error:badarg -> {error, {miss, eof}}
+    end.
 
-bump_ptr(Obj = #obj{ptr = Ptr}, N) ->
-    Obj#obj{ptr = Ptr+N}.
+bump_ptr(#obj{ptr = Ptr} = Obj, N) -> Obj#obj{ptr = Ptr+N}.
