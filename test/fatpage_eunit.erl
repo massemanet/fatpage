a4e3bb87833601824da0c937708891b204916f14
mats cronqvist <masse@cronqvi.st>
2022-12-16 15:30:12 +0000

    WiP bootstrap

--- test/fatpage_eunit.erl
+++ test/fatpage_eunit.erl
@@ -12,5 +12,13 @@ x_test_() ->
       demo:string("")),
    ?_assertEqual(
       {ok,[[<<"a">>]],eof},
-      demo:string("a"))
+      demo:string("a")),
+   ?_assertEqual(
+      {ok,[{rule,<<"a">>,
+            {alt,[{seq,[{rep,{1,1},<<"b">>},{rep,{0,inf},<<"B">>}]},
+                  {seq,[{rep,{1,inf},<<"c">>},{rep,{0,2},<<"C">>}]},
+                  {seq,[{rep,{2,3},<<"d">>},
+                        {rep,{1,1},
+                         {rep,{0,1},{alt,[{seq,[{rep,{1,1},<<"D">>}]}]}}}]}]}}],0},
+      fatpage_bootstrap_abnf:string("a=b *B/ 1*c *2C / 2*3d [D]\n"))
   ].
